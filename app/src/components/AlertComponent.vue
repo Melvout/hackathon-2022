b2d1f4c7bb1a2aa5374cacee0d2c0c208617ce04
Melvin NAMONT <melvin.namont@gmail.com>
2022-10-09 04:51:13 +0200

    animation alerte

--- app/src/components/AlertComponent.vue
+++ app/src/components/AlertComponent.vue
@@ -1,6 +1,6 @@
 <template>
   <v-container>
-    <v-table fixed-header height="300px">
+    <v-table fixed-header height="300px" style="overflow: hidden">
       <thead>
         <tr>
           <th class="text-left">Order</th>
@@ -11,34 +11,48 @@
         </tr>
       </thead>
       <tbody>
-        <tr
-          v-for="(alertOnOrder, index) in alertOnOrdersData"
-          :key="alertOnOrder.id"
+        <transition-group
+          name="custom-classes-transition"
+          enter-active-class="animated tada"
+          leave-active-class="animated bounceOutRight"
         >
-          <td>{{ alertOnOrder.id }}</td>
-          <td>{{ alertOnOrder.cause }}</td>
-          <td>{{ alertOnOrder.state }}</td>
-          <td>{{ alertOnOrder.date }}</td>
-          <td v-if="alertOnOrder.requiredAction == 0">Action 1</td>
-          <td v-else-if="alertOnOrder.requiredAction == 1">
-            <v-dialog id="modal" v-model="dialog" width="unset">
-              <template v-slot:activator="{ props }">
-                <v-btn icon size="25" v-bind="props">
-                  <v-icon size="20">mdi-exclamation</v-icon>
-                </v-btn>
-              </template>
+          <tr
+            v-for="(alertOnOrder, index) in alertOnOrdersData"
+            :key="alertOnOrder.id"
+          >
+            <td>{{ alertOnOrder.id }}</td>
+            <td>{{ alertOnOrder.cause }}</td>
+            <td>{{ alertOnOrder.state }}</td>
+            <td>{{ alertOnOrder.date }}</td>
+            <td v-if="alertOnOrder.requiredAction == 0">
+              <v-btn icon size="25" @click="validateModification(index)">
+                <v-icon size="20">mdi-check</v-icon>
+              </v-btn>
+            </td>
+            <td v-else-if="alertOnOrder.requiredAction == 1">
+              <v-dialog id="modal" v-model="dialog" width="unset">
+                <template v-slot:activator="{ props }">
+                  <v-btn icon size="25" v-bind="props">
+                    <v-icon size="20">mdi-exclamation</v-icon>
+                  </v-btn>
+                </template>
 
-              <ProposalsComponent
-                @proposals-component:close-dialog="clickOutside()"
-                @proposals-component:validate-modification="
-                  validateModification($event[0])
-                "
-                :alertId="index"
-              ></ProposalsComponent>
-            </v-dialog>
-          </td>
-          <td v-else-if="alertOnOrder.requiredAction == 2">Action 3</td>
-        </tr>
+                <ProposalsComponent
+                  @proposals-component:close-dialog="clickOutside()"
+                  @proposals-component:validate-modification="
+                    validateModification($event[0])
+                  "
+                  :alertId="index"
+                ></ProposalsComponent>
+              </v-dialog>
+            </td>
+            <td v-else-if="alertOnOrder.requiredAction == 2">
+              <v-btn icon size="25" @click="validateModification(index)">
+                <v-icon size="20">mdi-check</v-icon>
+              </v-btn>
+            </td>
+          </tr>
+        </transition-group>
       </tbody>
     </v-table>
   </v-container>
@@ -60,8 +74,18 @@ export default {
     },
     validateModification(alertId) {
       this.alertOnOrdersData[alertId].state = "Done";
+      this.alertOnOrdersData.splice(alertId, 1);
     },
   },
   components: { ProposalsComponent },
 };
 </script>
+<style>
+.v-table__wrapper {
+  overflow: hidden !important;
+}
+</style>
+
+<style scoped>
+@import url("https://cdn.jsdelivr.net/npm/animate.css@3.5.1");
+</style>>
