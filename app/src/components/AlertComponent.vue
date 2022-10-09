48fdb61c9c402b1c6abfbce11ba826350d3a2866
Melvin NAMONT <melvin.namont@gmail.com>
2022-10-09 05:20:19 +0200

    propre

--- app/src/components/AlertComponent.vue
+++ app/src/components/AlertComponent.vue
@@ -5,11 +5,11 @@
         <tr>
           <th class="text-left">Order</th>
           <th class="text-left">Cause</th>
-          <th class="text-left">Status</th>
           <th class="text-left">Date</th>
-          <th class="text-left">Required action</th>
+          <th class="th-center"></th>
         </tr>
       </thead>
+
       <tbody>
         <transition-group
           name="custom-classes-transition"
@@ -22,14 +22,13 @@
           >
             <td>{{ alertOnOrder.id }}</td>
             <td>{{ alertOnOrder.cause }}</td>
-            <td>{{ alertOnOrder.state }}</td>
             <td>{{ alertOnOrder.date }}</td>
-            <td v-if="alertOnOrder.requiredAction == 0">
+            <td class="td-center" v-if="alertOnOrder.requiredAction == 0">
               <v-btn icon size="25" @click="validateModification(index)">
                 <v-icon size="20">mdi-check</v-icon>
               </v-btn>
             </td>
-            <td v-else-if="alertOnOrder.requiredAction == 1">
+            <td class="td-center" v-else-if="alertOnOrder.requiredAction == 1">
               <v-dialog id="modal" v-model="dialog" width="unset">
                 <template v-slot:activator="{ props }">
                   <v-btn icon size="25" v-bind="props">
@@ -46,7 +45,7 @@
                 ></ProposalsComponent>
               </v-dialog>
             </td>
-            <td v-else-if="alertOnOrder.requiredAction == 2">
+            <td class="td-center" v-else-if="alertOnOrder.requiredAction == 2">
               <v-btn icon size="25" @click="validateModification(index)">
                 <v-icon size="20">mdi-check</v-icon>
               </v-btn>
@@ -88,4 +87,11 @@ export default {
 
 <style scoped>
 @import url("https://cdn.jsdelivr.net/npm/animate.css@3.5.1");
+
+.th-center,
+.td-center {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 </style>>
